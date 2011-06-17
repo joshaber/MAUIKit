a76cf80ae4122ff5760cdbaf39158eb4858c282d
joshaber <joshaber@gmail.com>
2011-06-16 17:32:47 -0700

    show two views side-by-side for comparison

--- Classes/MAUIKitAppDelegate.m
+++ Classes/MAUIKitAppDelegate.m
@@ -15,7 +15,8 @@
 - (void)animate1;
 - (void)animate2;
 
-@property (nonatomic, retain) MAUIView *buttonView;
+@property (nonatomic, retain) MAUIView *buttonView1;
+@property (nonatomic, retain) MAUIView *buttonView2;
 @end
 
 
@@ -25,20 +26,29 @@
 #pragma mark NSApplicationDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
-	self.buttonView = [[MAUIView alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 120.0f, 50.0f)];
-	self.buttonView.backgroundColor = [NSColor redColor];
-	self.buttonView.layer.cornerRadius = 5.0f;
-	self.buttonView.layer.masksToBounds = YES;
+	self.buttonView1 = [[MAUIView alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 140.0f, 50.0f)];
+	self.buttonView1.backgroundColor = [NSColor redColor];
+	self.buttonView1.layer.cornerRadius = 5.0f;
+	self.buttonView1.layer.masksToBounds = YES;
+	self.buttonView1.flattenSubviews = YES;
 	
-	MAUILabel *label = [[MAUILabel alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 120.0f, 50.0f)];
-	label.text = @"this is a test, eh?";
-	[self.buttonView addSubview:label];
+	MAUILabel *label1 = [[MAUILabel alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 140.0f, 50.0f)];
+	label1.text = @"Hi, I have subpixel anti-aliasing!";
+	[self.buttonView1 addSubview:label1];
 	
-	self.buttonView.flattenSubviews = YES;
+	self.buttonView2 = [[MAUIView alloc] initWithFrame:CGRectMake(10.0f, 80.0f, 140.0f, 50.0f)];
+	self.buttonView2.backgroundColor = [NSColor redColor];
+	self.buttonView2.layer.cornerRadius = 5.0f;
+	self.buttonView2.layer.masksToBounds = YES;
+	self.buttonView2.flattenSubviews = NO;
+	
+	MAUILabel *label2 = [[MAUILabel alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 140.0f, 50.0f)];
+	label2.text = @"Hi, I look like shit!";
+	[self.buttonView2 addSubview:label2];
 	
 	self.nsView.rootView.backgroundColor = [NSColor lightGrayColor];
-	[self.nsView.rootView addSubview:self.buttonView];
-	[self.nsView.rootView setNeedsDisplay];
+	[self.nsView.rootView addSubview:self.buttonView1];
+	[self.nsView.rootView addSubview:self.buttonView2];
 	
 	[self performSelector:@selector(animate1) withObject:nil afterDelay:5.0f];
 }
@@ -48,11 +58,13 @@
 
 @synthesize window;
 @synthesize nsView;
-@synthesize buttonView;
+@synthesize buttonView1;
+@synthesize buttonView2;
 
 - (void)animate1 {
 	[MAUIView beginAnimations:nil context:NULL];
-	self.buttonView.frame = CGRectMake(CGRectGetMaxX(self.nsView.rootView.bounds) - self.buttonView.frame.size.width*2, CGRectGetMaxY(self.nsView.rootView.bounds) - self.buttonView.frame.size.height*2, 120.0f, 50.0f);
+	self.buttonView1.frame = CGRectMake(CGRectGetMaxX(self.nsView.rootView.bounds) - self.buttonView1.frame.size.width*2, CGRectGetMaxY(self.nsView.rootView.bounds) - self.buttonView1.frame.size.height*2, self.buttonView1.bounds.size.width, self.buttonView1.bounds.size.height);
+	self.buttonView2.frame = CGRectMake(CGRectGetMaxX(self.nsView.rootView.bounds) - self.buttonView2.frame.size.width*2, CGRectGetMaxY(self.nsView.rootView.bounds) - self.buttonView2.frame.size.height, self.buttonView2.bounds.size.width, self.buttonView2.bounds.size.height);
 	[MAUIView commitAnimations];
 	
 	[self performSelector:@selector(animate2) withObject:nil afterDelay:5.0f];
@@ -60,7 +72,8 @@
 
 - (void)animate2 {
 	[MAUIView beginAnimations:nil context:NULL];
-	self.buttonView.alpha = 0.0f;
+	self.buttonView1.alpha = 0.0f;
+	self.buttonView2.alpha = 0.0f;
 	[MAUIView commitAnimations];
 }
 
